625fc5d1aeecaf4829f55e355f665f600f7e918c
MitchWatkins <student@csm-wl-dhcp-79-47.mines.edu>
2021-05-27 10:21:33 -0600

    Finished main GUI outline

--- SwiftIOApp/SwiftIOApp/ContentView.swift
+++ SwiftIOApp/SwiftIOApp/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var pinNum: String = ""
     @State private var voltage: String = ""
+    @State private var display: String = ""
     @State private var isEditing = false
     
     var body: some View {
@@ -23,22 +24,49 @@ struct ContentView: View {
                 Text("We out here").font(.subheadline)
             }
         }.padding()
-        
         VStack(alignment: .leading) {
+            Text("Enter pin number and voltage.")
+                .font(.title)
             TextField("Pin number", text: $pinNum) {
                 isEditing in
                 self.isEditing = isEditing
             } onCommit: {
-                self.pinNum = pinNum
+                self.pinNum = pinNum;
             }
             TextField("Voltage", text: $voltage) {
                 isEditing in
                 self.isEditing = isEditing
             } onCommit: {
-                self.voltage = voltage
+                self.voltage = voltage;
             }
             Button(action: {
-                print(pinNum + " " + voltage)
+                
+                // This is where the serial is called.
+                
+                print(pinNum + " " + voltage);
+            }, label: {
+                HStack {
+                    Image(systemName: "pencil")
+                    Text("Submit")
+                }
+            })
+            Spacer().frame(height: 10)
+            Divider()
+            Spacer().frame(height: 10)
+            Text("Enter text for the display.")
+                .font(.title)
+            TextField("Enter text here...", text: $display) {
+                isEditing in
+                self.isEditing = isEditing
+            } onCommit: {
+                self.display = display;
+            }
+            Button(action: {
+                
+                // This is where the serial is called.
+                
+                print("Yo");
+                print(pinNum + " " + voltage);
             }, label: {
                 HStack {
                     Image(systemName: "pencil")
