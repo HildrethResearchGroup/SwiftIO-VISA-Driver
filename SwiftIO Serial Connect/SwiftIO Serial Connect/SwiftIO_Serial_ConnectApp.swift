ae15d34a94bddf5181b025c6d7eb260fefa1a711
MitchWatkins <student@resnet-wl-dhcp-179-214.mines.edu>
2021-06-07 11:34:01 -0600

    Commit to make things work

---
+++ SwiftIO Serial Connect/SwiftIO Serial Connect/SwiftIO_Serial_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIO_Serial_ConnectApp.swift
+//  SwiftIO Serial Connect
+//
+//  Created by Mines CS Field Session Student on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIO_Serial_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
