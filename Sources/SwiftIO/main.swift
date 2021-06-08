7005ee3a88e04046b5c27e96cf2d6e612092711b
Ryan Rumana <56176109+rcrumana@users.noreply.github.com>
2021-06-08 09:45:09 -0600

    Template program for turning on LED
    proof of concept program for now

--- Sources/SwiftIO/main.swift
+++ Sources/SwiftIO/main.swift
@@ -1,30 +1,49 @@
-// Code for VISA Driver Project
-// Authors: Ryan, Ethan, Grant, Mitch
+// Code Template for VISA Driver Project
+// Authors: Grant Rulon, Ryan Rumana, Ethan Vijayabaskaran, and Mitch Watkins
 
-import SwiftIO
+// Project Imports //////////////////////////////////////////////////
+import SwiftIO						// Basic import for all programs
+// Add more here if needed
 
-// creating the UART object
-let uart = UART(Id.UART1)
-uart.clearBuffer()
 
-// creating the I2C object
-//let i2c = I2C(Id.I2C0)
-//let lcd = LCD1602(i2c)
+// Project Instance Variables ///////////////////////////////////////
+let uart = UART(Id.UART1)			// initializing the UART object for communication
+let i2c = I2C(Id.I2C0)				// initializing the I2C object for communication
+let lcd = LCD1602(i2c)				// initializing the LCD screen as a child of i2c
+let led = PWMOut(Id.PWM0A)			// initializing digital control of pin
+// Add more here if needed
 
+
+// Project setup ////////////////////////////////////////////////////
+uart.clearBuffer()					// clearing the serial buffer before first input
+lcd.clear()							// clearing the lcd screen before first input		
+// add any misc actions needed before the main while loop
+
+
+// Project while loop ///////////////////////////////////////////////
 while true {
-    sleep(ms: 2000)
-    uart.write("Intro Message: Hello world \n")
-    
+    // prompting the user
     uart.write("Read Character: ")
-    let input = uart.read(count: 5, timeout: -1)
-    uart.write("\n")
     
-    //uart.write("Output: \(input) \n")
-    lcd.write(x: 0, y: 0, input)
+    // waiting for a visa command
+    let input = uart.read(count: 8, timeout: -1)
     
-    uart.write("\n")
-    uart.write("\n")
-    uart.write("\n")
+    // UInt8 array to string conversion
+    var array = [String]()
+    for x in input {
+        let y = String(UnicodeScalar(UInt8(x)))
+        array.append(y)
+	}
+    let stringIn = array.joined()
+    
+    if(stringIn == "LED on  "){
+		led.setDutycycle(1)
+    } else if(stringIn == "LED off "){
+		led.setDutycycle(0.001)
+    } else {
+		lcd.write(x: 0, y: 0, stringIn)
+    }
+
+    uart.write("\n")    		// newline
+    
 }
-
-
